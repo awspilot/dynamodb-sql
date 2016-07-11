354bdf4b3f7937b8442724f19c250e40af666c78
databank <adrian@databank.ro>
2016-07-11 17:03:01 +0300

    REPLACE - updates

--- test/tests/050-replace.js
+++ test/tests/050-replace.js
@@ -2,17 +2,18 @@
 
 describe('replace', function () {
 	it('should replace when item exists', function(done) {
-		DynamoSQL.query(" REPLACE INTO `" + $tableName + "` " +
-						" SET " +
-						"	hash =  'hash1',      \n" +
-						"	\"range\" =  1,       \n" +
-						"   `Anumber`=1,           \n" +
-						"   `Aboolean`=true,       \n" +
-						"	`Anulled`=null,        \n" +
-						"   `Aarray`=[1,2, 3 ],      \n" +
-						"   `Aobject`= { aaa:1,bbb:2, ccc:\" some string \", ddd: {ddd1: 1}, eee: [1,'eee1']}, \n" +
-						"   `Astring_set`=:string_set, \n" +
-						"   `Anumber_set`=:number_set  \n", {}, function(err, data ) {
+		DynamoSQL.query("	REPLACE INTO `" + $tableName + "`        \n" +
+						"	SET                                      \n" +
+						"		`Anumber`=1,                         \n" +
+						"		`Aboolean`=true,                     \n" +
+						"		`Anulled`=null,                      \n" +
+						"		`Aarray`=[1,2, 3 ],                  \n" +
+						"		`Aobject`= { aaa:1,bbb:2, ccc:\" some string \", ddd: {ddd1: 1}, eee: [1,'eee1']}, \n" +
+						"		`Astring_set`=:string_set,           \n" +
+						"		`Anumber_set`=:number_set            \n" +
+						"	WHERE                                    \n" +
+						"		`hash` =  'hash1' AND `range` =  1   \n",  {}, function(err, data ) {
+
 			if (err)
 				throw err
 
@@ -33,12 +34,16 @@ describe('replace', function () {
 		})
 	})
 	it('should throw error on when item does not exist', function(done) {
+		//DynamoSQL.db.on('beforeRequest', console.log)
 		DynamoSQL.query(" REPLACE INTO `" + $tableName + "` " +
-						" SET " +
-						"	hash =  'hash_inexisting',      \n" +
-						"	range =  1            \n",
+						" SET                               " +
+						"	something = 1                   " +
+						" WHERE                             " +
+						"	hash =  'hash_inexisting' AND   " +
+						"	range =  1                      ",
 						{}, function(err, data ) {
 
+			//console.log("finished 2nd query", err, data )
 			if (err && err.code === 'ConditionalCheckFailedException')
 				return done()
 
