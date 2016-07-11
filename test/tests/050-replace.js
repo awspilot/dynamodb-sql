1f14a5f05712032128ca931c929d1acde6af19a3
databank <adrian@databank.ro>
2016-07-11 18:09:30 +0300

    FIX REPLACE, no need for WHERE

--- test/tests/050-replace.js
+++ test/tests/050-replace.js
@@ -4,15 +4,15 @@ describe('replace', function () {
 	it('should replace when item exists', function(done) {
 		DynamoSQL.query("	REPLACE INTO `" + $tableName + "`        \n" +
 						"	SET                                      \n" +
+						"		`hash` =  'hash1',                   \n" +
+						"		`range` =  1,                        \n" +
 						"		`Anumber`=1,                         \n" +
 						"		`Aboolean`=true,                     \n" +
 						"		`Anulled`=null,                      \n" +
 						"		`Aarray`=[1,2, 3 ],                  \n" +
 						"		`Aobject`= { aaa:1,bbb:2, ccc:\" some string \", ddd: {ddd1: 1}, eee: [1,'eee1']}, \n" +
 						"		`Astring_set`=:string_set,           \n" +
-						"		`Anumber_set`=:number_set            \n" +
-						"	WHERE                                    \n" +
-						"		`hash` =  'hash1' AND `range` =  1   \n",  {}, function(err, data ) {
+						"		`Anumber_set`=:number_set            \n",  {}, function(err, data ) {
 
 			if (err)
 				throw err
@@ -33,22 +33,19 @@ describe('replace', function () {
 				})
 		})
 	})
-	it('should throw error on when item does not exist', function(done) {
+	it('should replace when item does not exist', function(done) {
 		//DynamoSQL.db.on('beforeRequest', console.log)
 		DynamoSQL.query(" REPLACE INTO `" + $tableName + "` " +
 						" SET                               " +
-						"	something = 1                   " +
-						" WHERE                             " +
-						"	hash =  'hash_inexisting' AND   " +
-						"	range =  1                      ",
+						"	hash =  'hash_inexisting',      " +
+						"	range =  1,                     " +
+						"	something = 1                   ",
 						{}, function(err, data ) {
 
-			//console.log("finished 2nd query", err, data )
-			if (err && err.code === 'ConditionalCheckFailedException')
-				return done()
-
 			if (err)
-				throw err || 'expected to throw ConditionalCheckFailedException'
+				throw err
+
+			done()
 
 		})
 	})
