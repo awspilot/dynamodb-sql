b2a2ab8e04de6d01e8bbcbbd875d006613a3cbeb
databank <adrian@databank.ro>
2016-07-15 18:36:16 +0300

    tests: DELETE tests also for hash only table

--- test/tests/060-delete.js
+++ test/tests/060-delete.js
@@ -1,11 +1,36 @@
 
 describe('delete()', function () {
-	it('should deleate existing item', function(done) {
+	it('hash_range: should deleate existing item', function(done) {
 		DynamoSQL.query("delete from `" + $tableName + "` where `hash` = 'hash1' and range = 1", {}, function(err, data ) {
 			if (err) throw err
 
-			done()
+			DynamoSQL.db
+				.table($tableName)
+				.where('hash').eq('hash1')
+				.where('range').eq(1)
+				.get(function(err, item) {
+					if (err)
+						throw err
+
+					assert.deepEqual(item, {})
+					done()
+				})
 		})
 	})
+	it('hash: should deleate existing item', function(done) {
+		DynamoSQL.query("delete from `" + $hashTable + "` where `hash` = 'hash1'", {}, function(err, data ) {
+			if (err) throw err
 
+			DynamoSQL.db
+				.table($hashTable)
+				.where('hash').eq('hash1')
+				.get(function(err, item) {
+					if (err)
+						throw err
+
+					assert.deepEqual(item, {})
+					done()
+				})
+		})
+	})
 })
