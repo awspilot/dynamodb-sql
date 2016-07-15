266506a2155c6a0a5fece614b2e40be5fc8b6cee
databank <adrian@databank.ro>
2016-07-15 18:32:39 +0300

    tests: REPLACE tests also for hash only table

--- test/tests/050-replace.js
+++ test/tests/050-replace.js
@@ -1,7 +1,7 @@
 
 
 describe('replace', function () {
-	it('should replace when item exists', function(done) {
+	it('hash_range: should replace when item exists', function(done) {
 		DynamoSQL.query("	REPLACE INTO `" + $tableName + "`        \n" +
 						"	SET                                      \n" +
 						"		`hash` =  'hash1',                   \n" +
@@ -33,8 +33,39 @@ describe('replace', function () {
 				})
 		})
 	})
-	it('should replace when item does not exist', function(done) {
-		//DynamoSQL.db.on('beforeRequest', console.log)
+
+	it('hash: should replace when item exists', function(done) {
+		DynamoSQL.query("	REPLACE INTO `" + $hashTable + "`        \n" +
+						"	SET                                      \n" +
+						"		`hash` =  'hash1',                   \n" +
+						"		`Anumber`=1,                         \n" +
+						"		`Aboolean`=true,                     \n" +
+						"		`Anulled`=null,                      \n" +
+						"		`Aarray`=[1,2, 3 ],                  \n" +
+						"		`Aobject`= { aaa:1,bbb:2, ccc:\" some string \", ddd: {ddd1: 1}, eee: [1,'eee1']}, \n" +
+						"		`Astring_set`=:string_set,           \n" +
+						"		`Anumber_set`=:number_set            \n",  {}, function(err, data ) {
+
+			if (err)
+				throw err
+
+			DynamoSQL.db
+				.table($hashTable)
+				.where('hash').eq('hash1')
+				.get(function(err, item) {
+					if (err)
+						throw err
+
+					assert.equal(item.Anumber, 1)
+					assert.equal(item.Aboolean, true)
+					assert.equal(item.Anulled, null)
+					assert.deepEqual(item.Aarray, [1,2,3])
+					done()
+				})
+		})
+	})
+
+	it('hash_range: should replace when item does not exist', function(done) {
 		DynamoSQL.query(" REPLACE INTO `" + $tableName + "` " +
 						" SET                               " +
 						"	hash =  'hash_inexisting',      " +
@@ -45,7 +76,40 @@ describe('replace', function () {
 			if (err)
 				throw err
 
-			done()
+			DynamoSQL.db
+				.table($tableName)
+				.where('hash').eq('hash_inexisting')
+				.where('range').eq(1)
+				.get(function(err, item) {
+					if (err)
+						throw err
+
+					assert.equal(item.something, 1)
+					done()
+				})
+
+		})
+	})
+	it('hash: should replace when item does not exist', function(done) {
+		DynamoSQL.query(" REPLACE INTO `" + $hashTable + "` " +
+						" SET                               " +
+						"	hash =  'hash_inexisting',      " +
+						"	something = 1                   ",
+						{}, function(err, data ) {
+
+			if (err)
+				throw err
+
+			DynamoSQL.db
+				.table($hashTable)
+				.where('hash').eq('hash_inexisting')
+				.get(function(err, item) {
+					if (err)
+						throw err
+
+					assert.equal(item.something, 1)
+					done()
+				})
 
 		})
 	})
