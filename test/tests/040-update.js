e3eb96c65ac8e0ea1ca1e38cc0d282d60b0f566b
databank <adrian@databank.ro>
2016-07-15 18:26:44 +0300

    tests: update tests also for hash only table

--- test/tests/040-update.js
+++ test/tests/040-update.js
@@ -1,5 +1,5 @@
 describe('update', function () {
-	it('should update existing item', function(done) {
+	it('hash_range: should update existing item', function(done) {
 		DynamoSQL.query("update `" + $tableName + "` set `number`=2,`boolean`=false, `array`= [ 1,2,3,4 ], `new` = 'new_item', object= { key : {subkey: 1 } } where `hash` = 'hash1' and range = 1", {}, function(err, data ) {
 			if (err)
 				throw err
@@ -8,6 +8,29 @@ describe('update', function () {
 				.table($tableName)
 				.where('hash').eq('hash1')
 				.where('range').eq(1)
+				.get(function(err, item) {
+					if (err)
+						throw err
+
+					assert.equal(item.number, 2)
+					assert.equal(item.boolean, false)
+					assert.equal(item.nulled, null)
+					assert.deepEqual(item.array, [1,2,3,4])
+					assert.deepEqual(item.object, { key : {subkey: 1 } })
+
+					assert.equal(item.new, 'new_item')
+					done()
+				})
+		})
+	})
+	it('hash: should update existing item', function(done) {
+		DynamoSQL.query("update `" + $hashTable + "` set `number`=2,`boolean`=false, `array`= [ 1,2,3,4 ], `new` = 'new_item', object= { key : {subkey: 1 } } where `hash` = 'hash1' ", {}, function(err, data ) {
+			if (err)
+				throw err
+
+			DynamoSQL.db
+				.table($hashTable)
+				.where('hash').eq('hash1')
 				.get(function(err, item) {
 					if (err)
 						throw err
