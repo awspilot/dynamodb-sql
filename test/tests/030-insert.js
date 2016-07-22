4d34d8e0f5f3e3f9152d104c29b10fb2d3df3b12
databank <adrian@databank.ro>
2016-07-22 15:37:34 +0300

    support for comments

--- test/tests/030-insert.js
+++ test/tests/030-insert.js
@@ -3,19 +3,21 @@
 describe('insert', function () {
 	it('hash_range: should insert when item does not exist', function(done) {
 		DynamoSQL.query("\
-		 				INSERT INTO `" + $tableName + "`	\
-						SET									\
-							hash =  'hash1',				\
-							range =  1,						\
-							number = 1,						\
-							test_increment = 1,				\
-							boolean =true,					\
-							nulled = null,					\
-							array = [1,2, 3 ],				\
+						/* this is a comment */					\
+		 				INSERT INTO `" + $tableName + "`		\
+						SET										\
+							hash =  'hash1',					\
+							range =  1,							\
+							number = 1,							\
+							test_increment = 1,					\
+							test_comment = '  /* comment */ ',	\
+							boolean =true,						\
+							nulled = null,						\
+							array = [1,2, 3 ],					\
 							object= { aaa:1,bbb:2, ccc:\" some string \", ddd: {ddd1: 1}, eee: [1,'eee1']}, \
-							string_set=:string_set,			\
-							number_set=:number_set,			\
-							delete_me = 'delete_me'			\
+							string_set=:string_set,				\
+							number_set=:number_set,				\
+							delete_me = 'delete_me'				\
 						", {}, function(err, data ) {
 			if (err)
 				throw err
@@ -29,12 +31,14 @@ describe('insert', function () {
 						throw err
 
 					assert.equal(item.number, 1)
+					assert.equal(item.test_comment, '  /* comment */ ')
 					assert.equal(item.boolean, true)
 					assert.equal(item.nulled, null)
 					assert.deepEqual(item.array, [1,2,3])
 					done()
 				})
 		})
+
 	})
 	it('hash: should insert when item does not exist', function(done) {
 		DynamoSQL.query(" INSERT INTO `" + $hashTable + "` " +
