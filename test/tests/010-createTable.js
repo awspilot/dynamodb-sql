5a609eb1569541436c5ea084f9888271ece9c1d1
databank <adrian@databank.ro>
2016-08-23 13:22:31 +0300

    create table - cleanup sql first

--- test/tests/010-createTable.js
+++ test/tests/010-createTable.js
@@ -78,17 +78,16 @@ describe('CREATE TABLE', function () {
 	it('CREATE TABLE ' + $tableName + ' ', function(done) {
 		DynamoSQL.query("\
 						CREATE TABLE " + $tableName + " (										\
-							hash STRING,														\
-							range NUMBER,														\
+							hash STRING,range NUMBER,											\
 							gsi_string STRING,													\
 							gsi_number NUMBER,													\
 							lsi_string STRING,													\
 							lsi_number NUMBER,													\
-							PRIMARY KEY ( hash, range ) THROUGHPUT 5 5,							\
-							INDEX gsi_string GSI ( hash, gsi_string ) THROUGHPUT 5 5,			\
+							PRIMARY KEY(hash,range)THROUGHPUT 5 5,								\
+							INDEX gsi_string GSI ( hash, gsi_string)  THROUGHPUT 5 5,			\
 							INDEX gsi_number GSI ( hash, gsi_number ) PROJECTION KEYS_ONLY,		\
 							INDEX lsi_string LSI ( hash, lsi_string ) PROJECTION ALL ,		 	\
-							INDEX lsi_number LSI ( hash, lsi_number ) PROJECTION ( at1, at2 )	\
+							INDEX lsi_number LSI ( hash, lsi_number ) PROJECTION(at1,at2)		\
 						)																		\
 			", {}, function(err, data ) {
 				//console.log("reply from sql create table ",err, JSON.stringify(data,null,"\t"))
