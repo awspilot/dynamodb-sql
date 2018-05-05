ae43fae2bf1bc8234cfb19ac188e039b63948f74
databank <adrian@databank.ro>
2018-05-05 16:00:40 +0300

    update

--- test/index.js
+++ test/index.js
@@ -2,7 +2,8 @@ require('./lib/common')
 
 require("./tests/010-createTable.js")
 run_test('TABLES', 'test/res/020-showTables.yaml' )
-require("./tests/030-insert.js")
+run_test('INSERT', 'test/res/030-insert.yaml' )
+
 require("./tests/040-update.js")
 require("./tests/050-replace.js")
 require("./tests/060-delete.js")
