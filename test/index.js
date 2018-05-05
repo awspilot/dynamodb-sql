a111bd49e211945bf30af8b678209b4687e319de
databank <adrian@databank.ro>
2018-05-05 16:14:46 +0300

    update tests

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ run_test('INSERT', 'test/res/030-insert.yaml' )
 run_test('UPDATE', 'test/res/040-update.yaml' )
 
 require("./tests/050-replace.js")
-require("./tests/060-delete.js")
+run_test('DELETE', 'test/res/060-delete.yaml' )
 run_test('SELECT', 'test/res/080-query.yaml' )
 require("./tests/999-deleteTable.js")
 //require("./tests/090-scan.js")
