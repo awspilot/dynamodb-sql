ce5b21e44bd645fa9a92a7320c302cc8b6ea4a4c
databank <adrian@databank.ro>
2018-05-12 12:06:50 +0300

    empty replace tests

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ require("./tests/010-createTable.js")
 run_test('TABLES', 'test/res/020-showTables.yaml' )
 run_test('INSERT', 'test/res/030-insert.yaml' )
 run_test('UPDATE', 'test/res/040-update.yaml' )
-
+run_test('REPLACE', 'test/res/050-replace.yaml' )
 require("./tests/050-replace.js")
 run_test('DELETE', 'test/res/060-delete.yaml' )
 run_test('SELECT', 'test/res/080-query.yaml' )
